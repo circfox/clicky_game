b8abf930b2aa30c8946cd6584aaa8b480f0be88d
Ray Ventura <circfox@yahoo.com>
2018-11-06 23:48:05 -0800

    Updates

---
+++ precache-manifest.8651cb6b86f0c41225907f843cec1869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd845ee937b9e5f2a5a2",
+    "url": "/clicky_game/static/js/runtime~main.bd845ee9.js"
+  },
+  {
+    "revision": "78fc7dcf3beeacaf2185",
+    "url": "/clicky_game/static/js/main.78fc7dcf.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/clicky_game/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "78fc7dcf3beeacaf2185",
+    "url": "/clicky_game/static/css/main.a89d8a8b.chunk.css"
+  },
+  {
+    "revision": "6c3b13bf0cd9eee3884d2077f93ed192",
+    "url": "/clicky_game/index.html"
+  }
+];
